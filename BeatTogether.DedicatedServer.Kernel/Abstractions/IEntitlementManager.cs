c8eb739accdaf95e3150802de1bfe704e5c90166
MT <littlehappytroll@gmail.com>
2021-09-09 07:49:23 -0400

    forgot to commit this

--- BeatTogether.DedicatedServer.Kernel/Abstractions/IEntitlementManager.cs
+++ BeatTogether.DedicatedServer.Kernel/Abstractions/IEntitlementManager.cs
@@ -1,6 +1,18 @@
-﻿namespace BeatTogether.DedicatedServer.Kernel.Abstractions
+﻿using BeatTogether.DedicatedServer.Messaging.Enums;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace BeatTogether.DedicatedServer.Kernel.Abstractions
 {
     public interface IEntitlementManager
     {
+        ConcurrentDictionary<string, ConcurrentDictionary<string, EntitlementStatus>> userEntitlements { get; }
+
+        event Action<string, string, EntitlementStatus>? entitlementReceived;
+
+        bool AllPlayersHaveBeatmap(string beatmap);
+        List<string> GetPlayersWithoutBeatmap(string beatmap);
+        void HandleEntitlement(string userId, string beatmap, EntitlementStatus entitlement);
     }
 }
