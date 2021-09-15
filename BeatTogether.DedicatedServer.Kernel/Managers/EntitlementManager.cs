87c859f3580e3fb330f854ea23606002e4673890
MT <littlehappytroll@gmail.com>
2021-09-15 08:47:49 -0400

    fix entitlement manager

--- BeatTogether.DedicatedServer.Kernel/Managers/EntitlementManager.cs
+++ BeatTogether.DedicatedServer.Kernel/Managers/EntitlementManager.cs
@@ -11,15 +11,27 @@ namespace BeatTogether.DedicatedServer.Kernel.Managers
     {
         public ConcurrentDictionary<string, ConcurrentDictionary<string, EntitlementStatus>> userEntitlements { get; } = new();
 
+        private IPlayerRegistry _playerRegistry;
+
+        public EntitlementManager(
+            IPlayerRegistry playerRegistry)
+        {
+            _playerRegistry = playerRegistry;
+        }
+
+        // If all players are in the entitlement dictionary and have the song entitlement registered with 'OK'
         public bool AllPlayersHaveBeatmap(string beatmap)
-            => userEntitlements.Values.All(p => p.ContainsKey(beatmap) ? p[beatmap] == EntitlementStatus.Ok : false);
+            => _playerRegistry.Players.All(p => userEntitlements.TryGetValue(p.UserId, out var userEntitlement) && userEntitlement.TryGetValue(beatmap, out var entitlement) && entitlement == EntitlementStatus.Ok);
 
+        // If all players are in the entitlement dictionary and have a song entitlement other than 'NotOwned'
         public bool AllPlayersOwnBeatmap(string beatmap)
-            => userEntitlements.Values.All(p => p.ContainsKey(beatmap) ? p[beatmap] != EntitlementStatus.NotOwned : false);
+            => _playerRegistry.Players.All(p => userEntitlements.TryGetValue(p.UserId, out var userEntitlement) && userEntitlement.TryGetValue(beatmap, out var entitlement) && entitlement != EntitlementStatus.NotOwned);
 
+        // Gets a list of playerIds that have registered 'NotOwned' entitlement
         public List<string> GetPlayersWithoutBeatmap(string beatmap)
             => userEntitlements.Where(userEntitlement => userEntitlement.Value.ContainsKey(beatmap) && userEntitlement.Value[beatmap] == EntitlementStatus.NotOwned).Select(userEntitlement => userEntitlement.Key).ToList();
 
+        // Handles an entitlement
         public void HandleEntitlement(string userId, string beatmap, EntitlementStatus entitlement)
             => userEntitlements.GetOrAdd(userId, _ => new ConcurrentDictionary<string, EntitlementStatus>())[beatmap] = entitlement;
     }
