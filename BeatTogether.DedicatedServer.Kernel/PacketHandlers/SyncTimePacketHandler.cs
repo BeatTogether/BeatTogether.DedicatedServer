2b7ab80ae9d7dcbae2569625da25b8ef43ad5a4c
MT <littlehappytroll@gmail.com>
2021-11-15 12:43:30 -0500

    Respond with synctime packet

---
+++ BeatTogether.DedicatedServer.Kernel/PacketHandlers/SyncTimePacketHandler.cs
@@ -0,0 +1,35 @@
+﻿using BeatTogether.DedicatedServer.Kernel.Abstractions;
+using BeatTogether.DedicatedServer.Messaging.Packets;
+using LiteNetLib;
+using Serilog;
+using System.Threading.Tasks;
+
+namespace BeatTogether.DedicatedServer.Kernel.PacketHandlers
+{
+    public sealed class SyncTimePacketHandler : BasePacketHandler<SyncTimePacket>
+    {
+        private readonly IPacketDispatcher _packetDispatcher;
+        private readonly ILogger _logger = Log.ForContext<PlayerStatePacketHandler>();
+
+        public SyncTimePacketHandler(IPacketDispatcher packetDispatcher)
+        {
+            _packetDispatcher = packetDispatcher;
+        }
+
+        public override Task Handle(IPlayer sender, SyncTimePacket packet)
+        {
+            _logger.Debug(
+                $"Handling packet of type '{nameof(SyncTimePacket)}' " +
+                $"(SenderId={sender.ConnectionId}, SyncTime={packet.SyncTime})."
+            );
+
+            var syncTimePacket = new SyncTimePacket
+            {
+                SyncTime = sender.SyncTime
+            };
+            _packetDispatcher.SendToPlayer(sender, syncTimePacket, DeliveryMethod.ReliableOrdered);
+
+            return Task.CompletedTask;
+        }
+    }
+}
