43da19d697bcf0bda2007cca0bc17628041cb0ba
goobwabber <littlehappytroll@gmail.com>
2022-01-15 14:27:24 -0500

    forgot to update packet with encryption params

---
+++ BeatTogether.DedicatedServer.Messaging/Models/ByteArray.cs
@@ -0,0 +1,24 @@
+﻿using BeatTogether.LiteNetLib.Abstractions;
+using BeatTogether.LiteNetLib.Extensions;
+using Krypton.Buffers;
+using System;
+
+namespace BeatTogether.DedicatedServer.Messaging.Models
+{
+    public sealed class ByteArray : INetSerializable
+    {
+        public byte[]? Data { get; set; } = null;
+
+        public void ReadFrom(ref SpanBufferReader reader)
+        {
+            int length = (int)reader.ReadVarUInt();
+            Data = reader.ReadBytes(length).ToArray();
+        }
+
+        public void WriteTo(ref SpanBufferWriter writer)
+        {
+            writer.WriteVarUInt((Data != null) ? (uint)Data.Length : 0);
+            writer.WriteBytes(Data);
+        }
+    }
+}
--- BeatTogether.DedicatedServer.Messaging/Packets/PlayerIdentityPacket.cs
+++ BeatTogether.DedicatedServer.Messaging/Packets/PlayerIdentityPacket.cs
@@ -8,17 +8,23 @@ namespace BeatTogether.DedicatedServer.Messaging.Packets
     {
         public PlayerStateHash PlayerState { get; set; } = new();
         public AvatarData PlayerAvatar { get; set; } = new();
+        public ByteArray Random { get; set; } = new();
+        public ByteArray PublicEncryptionKey { get; set; } = new();
 
         public void ReadFrom(ref SpanBufferReader reader)
         {
             PlayerState.ReadFrom(ref reader);
             PlayerAvatar.ReadFrom(ref reader);
+            Random.ReadFrom(ref reader);
+            PublicEncryptionKey.ReadFrom(ref reader);
         }
 
         public void WriteTo(ref SpanBufferWriter writer)
         {
             PlayerState.WriteTo(ref writer);
             PlayerAvatar.WriteTo(ref writer);
+            Random.WriteTo(ref writer);
+            PublicEncryptionKey.WriteTo(ref writer);
         }
     }
 }
