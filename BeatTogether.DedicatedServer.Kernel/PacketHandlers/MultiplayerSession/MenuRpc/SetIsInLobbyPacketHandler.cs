b42eb3b3ce40adb39a47289d1c136d20267d88a8
goobwabber <littlehappytroll@gmail.com>
2022-02-11 14:59:39 -0500

    fix start button enabled when no song selected
    also line endings

--- BeatTogether.DedicatedServer.Kernel/PacketHandlers/MultiplayerSession/MenuRpc/SetIsInLobbyPacketHandler.cs
+++ BeatTogether.DedicatedServer.Kernel/PacketHandlers/MultiplayerSession/MenuRpc/SetIsInLobbyPacketHandler.cs
@@ -1,5 +1,7 @@
 ﻿using BeatTogether.DedicatedServer.Kernel.Abstractions;
+using BeatTogether.DedicatedServer.Messaging.Enums;
 using BeatTogether.DedicatedServer.Messaging.Packets.MultiplayerSession.MenuRpc;
+using BeatTogether.LiteNetLib.Enums;
 using Serilog;
 using System.Threading.Tasks;
 
@@ -7,7 +9,14 @@ namespace BeatTogether.DedicatedServer.Kernel.PacketHandlers.MultiplayerSession.
 {
     public sealed class SetIsInLobbyPacketHandler : BasePacketHandler<SetIsInLobbyPacket>
     {
-        private ILogger _logger = Log.ForContext<SetIsInLobbyPacketHandler>();
+        private readonly PacketDispatcher _packetDispatcher;
+        private readonly ILogger _logger = Log.ForContext<SetIsInLobbyPacketHandler>();
+
+        public SetIsInLobbyPacketHandler(
+            PacketDispatcher packetDispatcher)
+        {
+            _packetDispatcher = packetDispatcher;
+        }
 
         public override Task Handle(IPlayer sender, SetIsInLobbyPacket packet)
         {
@@ -15,6 +24,12 @@ namespace BeatTogether.DedicatedServer.Kernel.PacketHandlers.MultiplayerSession.
                 $"Handling packet of type '{nameof(SetIsInLobbyPacket)}' " +
                 $"(SenderId={sender.ConnectionId}, InLobby={packet.IsInLobby})."
             );
+
+            if (packet.IsInLobby && !sender.InLobby)
+                _packetDispatcher.SendToPlayer(sender, new SetIsStartButtonEnabledPacket
+                {
+                    Reason = CannotStartGameReason.NoSongSelected
+                }, DeliveryMethod.ReliableOrdered);
 
             sender.InLobby = packet.IsInLobby;
             return Task.CompletedTask;
--- BeatTogether.DedicatedServer.Kernel/PacketHandlers/PlayerStatePacketHandler.cs
+++ BeatTogether.DedicatedServer.Kernel/PacketHandlers/PlayerStatePacketHandler.cs
@@ -1,6 +1,9 @@
 ﻿using BeatTogether.DedicatedServer.Kernel.Abstractions;
 using BeatTogether.DedicatedServer.Kernel.Managers.Abstractions;
+using BeatTogether.DedicatedServer.Messaging.Enums;
 using BeatTogether.DedicatedServer.Messaging.Packets;
+using BeatTogether.DedicatedServer.Messaging.Packets.MultiplayerSession.MenuRpc;
+using BeatTogether.LiteNetLib.Enums;
 using Serilog;
 using System.Threading.Tasks;
 
@@ -8,14 +11,7 @@ namespace BeatTogether.DedicatedServer.Kernel.PacketHandlers
 {
     public sealed class PlayerStatePacketHandler : BasePacketHandler<PlayerStatePacket>
     {
-        private readonly ILobbyManager _lobbyManager;
         private readonly ILogger _logger = Log.ForContext<PlayerStatePacketHandler>();
-
-        public PlayerStatePacketHandler(
-            ILobbyManager lobbyManager)
-        {
-            _lobbyManager = lobbyManager;
-        }
 
         public override Task Handle(IPlayer sender, PlayerStatePacket packet)
         {
