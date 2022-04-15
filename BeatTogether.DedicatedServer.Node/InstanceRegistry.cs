09845dde65c464f61ce748fe456f12820e4eb3c4
cubic <cubicgraphics98@gmail.com>
2022-04-15 18:06:31 +0100

    oops

--- BeatTogether.DedicatedServer.Node/InstanceRegistry.cs
+++ BeatTogether.DedicatedServer.Node/InstanceRegistry.cs
@@ -20,31 +20,5 @@ namespace BeatTogether.DedicatedServer.Node
 
         public bool TryGetInstance(string secret, [MaybeNullWhen(false)] out IDedicatedInstance instance) =>
             _instances.TryGetValue(secret, out instance);
-
-
-
-
-        public int GetPlayerCount(bool Ingame)
-        {
-            IDedicatedInstance[] instances = ((IDedicatedInstance[])_instances.Values);
-            int total = 0;
-            for (int i = 0; i < instances.Length; i++)
-            {
-                switch (Ingame)
-                {
-                    case true:
-                        total += instances[i].PlayerCountInGame;
-                        break;
-                    case false:
-                        total += instances[i].PlayerCount;
-                        break;
-                }
-            }
-            return total;
-        }
-        public int GetInstanceCount()
-        {
-            return ((IDedicatedInstance[])_instances.Values).Length + 1;
-        }
     }
 }
