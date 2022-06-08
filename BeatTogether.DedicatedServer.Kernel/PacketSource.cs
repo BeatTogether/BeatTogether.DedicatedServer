1a36b68f16b4b37cf702f93b2e7353eda8357cc6
goobwabber <littlehappytroll@gmail.com>
2022-06-08 17:31:52 -0400

    remove most useless log ever

--- BeatTogether.DedicatedServer.Kernel/PacketSource.cs
+++ BeatTogether.DedicatedServer.Kernel/PacketSource.cs
@@ -106,7 +106,7 @@ namespace BeatTogether.DedicatedServer.Kernel
                 var packetHandler = _serviceProvider.GetService(packetHandlerType);
                 if (packetHandler is null)
                 {
-                    _logger.Verbose($"No handler exists for packet of type '{packetType.Name}'.");
+                    //_logger.Verbose($"No handler exists for packet of type '{packetType.Name}'.");
 
                     // skip any unprocessed bytes
                     var processedBytes = reader.Offset - prevPosition;
