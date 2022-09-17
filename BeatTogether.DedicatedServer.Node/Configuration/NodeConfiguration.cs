8e01107ce38657fad8d39115f3edacc21d87027b
cubic <cubicgraphics98@gmail.com>
2022-09-17 15:56:30 +0100

    bump ver

--- BeatTogether.DedicatedServer.Node/Configuration/NodeConfiguration.cs
+++ BeatTogether.DedicatedServer.Node/Configuration/NodeConfiguration.cs
@@ -3,7 +3,7 @@
     public sealed class NodeConfiguration
     {
         public string HostName { get; set; } = "127.0.0.1";
-        public string NodeVersion { get; } = "1.2.2";
+        public string NodeVersion { get; } = "1.2.3";
         public int BasePort { get; set; } = 30000;
         public int MaximumSlots { get; set; } = 10000;
     }
