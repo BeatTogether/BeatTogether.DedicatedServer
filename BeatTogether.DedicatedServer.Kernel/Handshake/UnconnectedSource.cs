f6489c558543d3da291c2443112ad984e09ef323
Roy de Jong <roy@softwarepunt.nl>
2023-04-08 16:53:50 +0200

    wip(1.29): handshake reliable request acks

--- BeatTogether.DedicatedServer.Kernel/Handshake/UnconnectedSource.cs
+++ BeatTogether.DedicatedServer.Kernel/Handshake/UnconnectedSource.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using System.Net;
 using BeatTogether.Core.Messaging.Abstractions;
+using BeatTogether.Core.Messaging.Messages;
 using BeatTogether.DedicatedServer.Kernel.Abstractions;
 using BeatTogether.LiteNetLib.Enums;
 using BeatTogether.LiteNetLib.Sources;
@@ -13,18 +14,18 @@ namespace BeatTogether.DedicatedServer.Kernel.Handshake
     public class UnconnectedSource : UnconnectedMessageSource
     {
         private readonly IServiceProvider _serviceProvider;
-        private readonly IHandshakeService _handshakeService;
+        private readonly IUnconnectedDispatcher _unconnectedDispatcher;
         private readonly IMessageReader _messageReader;
 
         private readonly ConcurrentDictionary<EndPoint, HandshakeSession> _sessions;
 
         private readonly ILogger _logger;
 
-        public UnconnectedSource(IServiceProvider serviceProvider, IHandshakeService handshakeService,
+        public UnconnectedSource(IServiceProvider serviceProvider, IUnconnectedDispatcher unconnectedDispatcher,
             IMessageReader messageReader)
         {
             _serviceProvider = serviceProvider;
-            _handshakeService = handshakeService;
+            _unconnectedDispatcher = unconnectedDispatcher;
             _messageReader = messageReader;
 
             _sessions = new();
@@ -47,6 +48,7 @@ namespace BeatTogether.DedicatedServer.Kernel.Handshake
         {
             var messageType = message.GetType();
 
+            // Skip previously handled messages
             if (message is IRequest request && !session.ShouldHandleRequest(request.RequestId))
             {
                 _logger.Warning("Skipping duplicate request (MessageType={Type}, RequestId={RequestId})",
@@ -55,9 +57,20 @@ namespace BeatTogether.DedicatedServer.Kernel.Handshake
                 return;
             }
 
-            _logger.Information("Handling handshake message {MessageType} (EndPoint={EndPoint})",
-                messageType.Name, session.EndPoint.ToString());
+            // Acknowledge reliable messages
+            if (message is IReliableRequest reliableRequest)
+            {
+                _unconnectedDispatcher.Send(session, new AcknowledgeMessage()
+                {
+                    ResponseId = reliableRequest.RequestId,
+                    MessageHandled = true
+                });
+            }
 
+            // Dispatch to handler
+            _logger.Information("Handling handshake message of type {MessageType} (EndPoint={EndPoint})",
+                messageType.Name, session.EndPoint.ToString());
+            
             var targetHandlerType = typeof(IHandshakeMessageHandler<>).MakeGenericType(messageType);
             var messageHandler = _serviceProvider.GetService(targetHandlerType);
 
