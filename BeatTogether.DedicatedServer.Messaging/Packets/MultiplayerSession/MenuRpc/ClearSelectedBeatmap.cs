4b7584c1614c98d9629cfd49fdcf105d2d9bbef1
cubic <cubicgraphics98@gmail.com>
2023-04-12 23:19:19 +0100

    This works

--- BeatTogether.DedicatedServer.Messaging/Packets/MultiplayerSession/MenuRpc/ClearSelectedBeatmap.cs
+++ BeatTogether.DedicatedServer.Messaging/Packets/MultiplayerSession/MenuRpc/ClearSelectedBeatmap.cs
@@ -8,7 +8,6 @@ namespace BeatTogether.DedicatedServer.Messaging.Packets.MultiplayerSession.Menu
         public override void WriteTo(ref SpanBufferWriter writer)
         {
             base.WriteTo(ref writer);
-            writer.WriteInt8(0); //Idk why but seems to be an issue with this packet's size being off by 1 too small or 3 too big. im going with 1 too small for now
         }
     }
 }
