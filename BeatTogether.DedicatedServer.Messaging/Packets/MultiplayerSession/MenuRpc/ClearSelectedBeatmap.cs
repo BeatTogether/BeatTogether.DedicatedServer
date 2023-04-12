119ffab55be30d1b1adbf73f6f3d1a4a7f982d48
cubic <cubicgraphics98@gmail.com>
2023-04-12 23:01:38 +0100

    populated the clearselectedbeatmap packet with a single byte to increase its packet size for now

--- BeatTogether.DedicatedServer.Messaging/Packets/MultiplayerSession/MenuRpc/ClearSelectedBeatmap.cs
+++ BeatTogether.DedicatedServer.Messaging/Packets/MultiplayerSession/MenuRpc/ClearSelectedBeatmap.cs
@@ -1,8 +1,14 @@
 ﻿using BeatTogether.DedicatedServer.Messaging.Abstractions;
+using Krypton.Buffers;
 
 namespace BeatTogether.DedicatedServer.Messaging.Packets.MultiplayerSession.MenuRpc
 {
     public sealed class ClearSelectedBeatmap : BaseRpcWithValuesPacket
     {
+        public override void WriteTo(ref SpanBufferWriter writer)
+        {
+            base.WriteTo(ref writer);
+            writer.WriteInt8(0); //Idk why but seems to be an issue with this packet's size being off by 1 too small or 3 too big. im going with 1 too small for now
+        }
     }
 }
