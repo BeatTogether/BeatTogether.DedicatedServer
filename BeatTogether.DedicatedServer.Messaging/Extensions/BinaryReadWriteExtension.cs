7d8ac0580f0b833ba428c3263f6a44ca54dfb8f4
EnderdracheLP <enderdrachelp@elp.quest>
2023-12-18 14:17:45 +0100

    Add necessary extension methods

---
+++ BeatTogether.DedicatedServer.Messaging/Extensions/BinaryReadWriteExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using BeatTogether.DedicatedServer.Messaging.Models;
+
+namespace BeatTogether.DedicatedServer.Messaging.Extensions
+{
+    public static class BinaryReadWriteExtension
+    {
+        // Token: 0x06000069 RID: 105 RVA: 0x00002ED8 File Offset: 0x000010D8
+        public static void Write(this BinaryWriter binaryWriter, Color color)
+        {
+            binaryWriter.Write(color.r);
+            binaryWriter.Write(color.g);
+            binaryWriter.Write(color.b);
+            binaryWriter.Write(color.a);
+        }
+
+        // Token: 0x0600006A RID: 106 RVA: 0x00002F0A File Offset: 0x0000110A
+        public static Color ReadColor(this BinaryReader binaryReader)
+        {
+            return new Color(binaryReader.ReadSingle(), binaryReader.ReadSingle(), binaryReader.ReadSingle(), binaryReader.ReadSingle());
+        }
+    }
+}
---
+++ BeatTogether.DedicatedServer.Messaging/Extensions/SpanBufferReaderExtensions.cs
@@ -0,0 +1,52 @@
+﻿using BeatTogether.DedicatedServer.Messaging.Models;
+using Krypton.Buffers;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BeatTogether.Extensions
+{
+    public static class SpanBufferReaderExtensions
+    {
+        public static (byte SenderId, byte ReceiverId, byte PacketOption) ReadRoutingHeader(this ref SpanBufferReader reader) =>
+            (reader.ReadUInt8(), reader.ReadUInt8(), reader.ReadUInt8());
+
+        public static bool TryReadRoutingHeader(this ref SpanBufferReader reader, [MaybeNullWhen(false)] out (byte SenderId, byte ReceiverId, byte PacketOption) routingHeader)
+        {
+            if (reader.RemainingSize < 2)
+            {
+                routingHeader = default;
+                return false;
+            }
+
+            routingHeader = (reader.ReadUInt8(), reader.ReadUInt8(), reader.ReadUInt8());
+            return true;
+        }
+
+        public static void ReadBytesArray(this ref SpanBufferReader reader, ref byte[]? array)
+        {
+            int length = (int)reader.ReadVarUInt();
+            array = reader.ReadBytes(length).ToArray();
+        }
+
+        public static void ReadColor(this ref SpanBufferReader reader, ref Color color)
+        {
+            color.r = reader.ReadFloat32();
+            color.g = reader.ReadFloat32();
+            color.b = reader.ReadFloat32();
+            color.a = reader.ReadFloat32();
+        }
+
+        public static void ReadColorNoAlpha(this ref SpanBufferReader reader, ref ColorNoAlpha color)
+        {
+            color.r = reader.ReadFloat32();
+            color.g = reader.ReadFloat32();
+            color.b = reader.ReadFloat32();
+        }
+
+        public static void ReadBeatmapIdentifier(this ref SpanBufferReader reader, ref BeatmapIdentifier beatmapIdentifier)
+        {
+            beatmapIdentifier.LevelId = reader.ReadString();
+            beatmapIdentifier.Characteristic = reader.ReadString();
+            beatmapIdentifier.Difficulty = (BeatmapDifficulty)reader.ReadVarUInt();
+        }
+    }
+}
