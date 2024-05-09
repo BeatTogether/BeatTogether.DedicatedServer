c453ba7d2b9e7fee8523abfd7474eac8f759db23
cubic <cubicgraphics98@gmail.com>
2024-05-09 23:27:44 +0100

    Fix IP adress of nodes

--- BeatTogether.DedicatedServer.Instancing/Configuration/InstancingConfiguration.cs
+++ BeatTogether.DedicatedServer.Instancing/Configuration/InstancingConfiguration.cs
@@ -4,7 +4,8 @@ namespace BeatTogether.DedicatedServer.Instancing.Configuration
 {
     public sealed class InstancingConfiguration
     {
-        public string HostName { get; set; } = "192.168.0.21";
+        //public string HostName { get; set; } = "192.168.0.21";
+        public string HostName { get; set; } = "127.0.0.1";
         public int BasePort { get; set; } = 30000;
         public int MaximumSlots { get; set; } = 10000;
     }
--- BeatTogether.DedicatedServer.Node/Configuration/NodeConfiguration.cs
+++ BeatTogether.DedicatedServer.Node/Configuration/NodeConfiguration.cs
@@ -4,7 +4,8 @@ namespace BeatTogether.DedicatedServer.Node.Configuration
 {
     public sealed class NodeConfiguration
     {
-        public string HostName { get; set; } = "192.168.0.21";
+        //public string HostName { get; set; } = "192.168.0.21";
+        public string HostName { get; set; } = "127.0.0.1";
         public Version NodeVersion { get; } = new Version(2,0,0);
     }
 }
