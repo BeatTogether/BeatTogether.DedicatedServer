dfc327dc13d88160257b14104b65bc2124fb16ca
EnderdracheLP <enderdrachelp@elp.quest>
2024-06-16 15:59:54 +0200

    Fix spacing for log line

--- BeatTogether.DedicatedServer.Kernel/PacketHandlers/PlayerStatePacketHandler.cs
+++ BeatTogether.DedicatedServer.Kernel/PacketHandlers/PlayerStatePacketHandler.cs
@@ -19,10 +19,10 @@ namespace BeatTogether.DedicatedServer.Kernel.PacketHandlers
         {
             _logger.Debug(
                 $"Handling packet of type '{nameof(PlayerStatePacket)}' " +
-                $"(SenderId={sender.ConnectionId}, IsPlayer={packet.PlayerState.Contains("player")}" + 
-                $"IsActive={packet.PlayerState.Contains("is_active")}, WantsToPlayNextLevel={packet.PlayerState.Contains("wants_to_play_next_level")}" +
-                $"IsSpectating={packet.PlayerState.Contains("spectating")}, InMenu={packet.PlayerState.Contains("in_menu")}" +
-                $"backgrounded={packet.PlayerState.Contains("backgrounded")}, in_gameplay={packet.PlayerState.Contains("in_gameplay")}" +
+                $"(SenderId={sender.ConnectionId}, IsPlayer={packet.PlayerState.Contains("player")}, " + 
+                $"IsActive={packet.PlayerState.Contains("is_active")}, WantsToPlayNextLevel={packet.PlayerState.Contains("wants_to_play_next_level")}, " +
+                $"IsSpectating={packet.PlayerState.Contains("spectating")}, InMenu={packet.PlayerState.Contains("in_menu")}, " +
+                $"backgrounded={packet.PlayerState.Contains("backgrounded")}, in_gameplay={packet.PlayerState.Contains("in_gameplay")}, " +
                 $"was_active_at_level_start={packet.PlayerState.Contains("was_active_at_level_start")}, finished_level={packet.PlayerState.Contains("finished_level")})."
             );
 
